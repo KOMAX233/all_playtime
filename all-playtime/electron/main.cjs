c14090a25451a116659b3c69d7532169bded5c7d
KOMAX233 <fajasmine@outlook.com>
2025-11-01 23:22:35 -0400

    add electron to make vite react run in it

---
+++ all-playtime/electron/main.cjs
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let win;
+function create() {
+  win = new BrowserWindow({
+    width: 1100,
+    height: 760,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  const devUrl = process.env.ELECTRON_START_URL;
+  if (devUrl) {
+    win.loadURL(devUrl);
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(create);
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
+app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) create(); });
